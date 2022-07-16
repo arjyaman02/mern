25078e2ca53f7fdd65035abcd895131d7893e39c
arjyaman02 <108915565+arjyaman02@users.noreply.github.com>
2022-07-16 10:51:57 +0530

    Add files via upload

---
+++ assignments/ass9 github/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');   
+const app = express();
+
+const port = 4000;
+
+app.get('/',(req,res)=>{
+    const content = JSON.parse(fs.readFileSync("product.json"));
+    res.status(200).json({
+        massage : "CART INFORMATION !",
+        data : content
+   })
+})
+app.get('/:product_id',(req,res)=>{
+    product_id = req.params.product_id;
+    const content = JSON.parse(fs.readFileSync("product.json"));
+    const matcheddata = content.filter(item=>item.product_id == product_id);
+    res.status(200).json({
+         massage : "CART INFORMATION !",
+         data : matcheddata
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`Server started at port ${port}`);
+});
